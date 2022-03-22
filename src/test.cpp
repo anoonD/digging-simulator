c5c58f8a502a1d8b085dec73fbf2caf31df37b2f
Anoond <anoond@cock.li>
2022-03-21 22:07:19 -0400

    hi

--- src/test.cpp
+++ src/test.cpp
@@ -1,10 +1,58 @@
 #include <fmt/color.h>
+#include <chrono>
+#include <thread>
+#include <iostream>
+
+#define STEP 5
+#define TIME 100
+#define ROWS 20
+#define COLS 20
+
+inline void clear() {
+    int x = system("clear");
+    if(x != 0) {
+        fmt::print("An error occured with the clear function.\n");
+        exit(1);
+    }
+}
+
+void increment(int &color, int &step) {
+    if(color+step >= 255) {
+        step = -step;
+    }
+    else if(color-abs(step) <= step) {
+        color=2*abs(step);
+        step = -step;
+    }
+
+    color += step;
+
+}
 
 int main() {
-    fmt::color color2 = {
-    };
+    int r{0}, g{125}, b{255},
+    rstep{STEP}, gstep{STEP}, bstep{STEP};
+    std::string symbol = "██";
 
-    fmt::print("{}",
-        fmt::styled("Normal", fg(fmt::rgb(200, 200, 200)) | fmt::emphasis::bold )
-    );
+    while(true) {
+        increment(r, rstep);
+        increment(g, gstep);
+        increment(b, bstep);
+
+        fmt::print(
+            "r: {}\n"
+            "g: {}\n"
+            "b: {}\n", r, g, b);
+
+        for(int x=0; x<ROWS; x++) {
+            for(int y=0; y<COLS; y++) {
+                fmt::print(fg(fmt::rgb(r*(x+1), g, b)), "{}", symbol);
+            }
+            fmt::print("\n");
+        }
+        
+        std::this_thread::sleep_for(std::chrono::milliseconds(TIME));
+
+        clear();
+    }
 }
